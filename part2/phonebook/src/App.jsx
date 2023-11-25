9308b6a222a1bae49d3595ff8b49bc364feb1140
Tayyeba Shoaib <tayyeba130@gmail.com>
2023-11-25 18:15:08 +0000

    delete long list

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,9 +6,6 @@ import { Persons } from "./components/Persons";
 const App = () => {
 	const [persons, setPersons] = useState([
 		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
 	]);
 	const [values, setValues] = useState({ newName: "", newNumber: "" });
 	const [filterText, setFilterText] = useState("");
