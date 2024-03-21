d244e841cf8a1def140a80a3b3aaa36a9c252ed9
Tayyeba Shoaib <tayyeba130@gmail.com>
2024-03-21 17:14:54 +0000

    complete 3a

--- part3/phonebook_be/index.js
+++ part3/phonebook_be/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+// Define a custom token to log request body
+morgan.token("req-body", function (req) {
+	return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, "content-length"),
+			"-",
+			tokens["response-time"](req, res),
+			"ms",
+			tokens["req-body"](req, res),
+		].join(" ");
+	})
+);
 
 let persons = [
 	{
