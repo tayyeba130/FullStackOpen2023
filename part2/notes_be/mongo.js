f8381ba0fc777ba711d5231069505f93ee1e243f
Tayyeba Shoaib <tayyeba130@gmail.com>
2024-03-30 11:13:56 +0000

    add mongo

--- part2/notes_be/mongo.js
+++ part2/notes_be/mongo.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-	console.log(
-		"Please provide the password as an argument: node mongo.js <password>"
-	);
+	console.log("give password as argument");
 	process.exit(1);
 }
 
-const password = process.env[2];
+const password = process.argv[2];
 
-const url = `mongodb+srv://tayyebat:${password}@cluster0.btnnti7.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://tayyebat:${password}@cluster0.btnnti7.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
 
@@ -22,12 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-	content: "HTML is Easy",
-	important: true,
-});
+// const note = new Note({
+// 	content: "React is a must",
+// 	important: true,
+// });
 
-note.save().then((result) => {
-	console.log("note saved!");
+// note.save().then((result) => {
+// 	console.log("note saved!");
+// 	mongoose.connection.close();
+// });
+
+Note.find({ important: true }).then((notes) => {
+	notes.forEach((note) => {
+		console.log(note);
+	});
 	mongoose.connection.close();
 });
