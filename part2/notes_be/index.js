2bed2c274b303059ffb044403b555ef1d37ede19
Tayyeba Shoaib <tayyeba130@gmail.com>
2024-04-13 13:58:01 +0100

    delete note BE

--- part2/notes_be/index.js
+++ part2/notes_be/index.js
@@ -1,53 +1,52 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
+
 const app = express();
 
 app.use(express.static("dist"));
+
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:  ", request.path);
+	console.log("Body:  ", request.body);
+	console.log("---");
+	next();
+};
+
 app.use(cors());
 app.use(express.json());
-
-let notes = [
-	{
-		id: 1,
-		content: "HTML is easy",
-		important: true,
-	},
-	{
-		id: 2,
-		content: "Browser can execute only JavaScript",
-		important: false,
-	},
-	{
-		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		important: true,
-	},
-];
+app.use(requestLogger);
 
 app.get("/", (req, res) => {
 	res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (req, res) => {
-	res.json(notes);
+	Note.find({}).then((notes) => {
+		res.json(notes);
+	});
 });
 
-app.get("/api/notes/:id", (req, res) => {
-	const id = Number(req.params.id);
-	const note = notes.find((note) => note.id === id);
-	if (note) {
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
+app.get("/api/notes/:id", (req, res, next) => {
+	Note.findById(req.params.id)
+		.then((note) => {
+			if (note) {
+				res.json(note);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
-// ruAtZok5GcIQHaNS
-// mongodb+srv://tayyebat:ruAtZok5GcIQHaNS@cluster0.btnnti7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// mongodb+srv://tayyebat:<password>@cluster0.btnnti7.mongodb.net/
-app.delete("/api/notes/:id", (req, res) => {
-	const id = Number(req.params.id);
-	notes = notes.filter((note) => note.id !== id);
-	res.status(204).end();
+
+app.delete("/api/notes/:id", (req, res, next) => {
+	Note.findByIdAndDelete(req.params.id)
+		.then((result) => {
+			res.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post("/api/notes", (req, res) => {
@@ -59,25 +58,51 @@ app.post("/api/notes", (req, res) => {
 		});
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
-		important: Boolean(body.important) || false,
+		important: body.important || false,
 		// when the important property is false, then the body.important || false expression
 		// will in fact return the false from the right-hand side
-		id: generateID(),
-	};
+	});
 
-	notes = notes.concat(note);
-
-	res.json(note);
+	note.save().then((savedNote) => {
+		res.json(savedNote);
+	});
 });
+
+app.put("/api/notes/:id", (req, res, next) => {
+	const body = req.body;
+	const note = {
+		content: body.content,
+		important: body.important,
+	};
+	Note.findByIdAndUpdate(req.params.id, note, { new: true })
+		.then((updatedNote) => {
+			res.json(updatedNote);
+		})
+		.catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
-function generateID() {
-	// The array is transformed into individual numbers by using the ... operator
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-}
--- part2/notes_be/mongo.js
+++ part2/notes_be/mongo.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
@@ -7,7 +8,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://tayyebat:${password}@cluster0.btnnti7.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set("strictQuery", false);
 
@@ -20,19 +21,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-// const note = new Note({
-// 	content: "React is a must",
-// 	important: true,
-// });
+const note = new Note({
+	content: "React is a must",
+	important: true,
+});
 
-// note.save().then((result) => {
-// 	console.log("note saved!");
-// 	mongoose.connection.close();
-// });
-
-Note.find({ important: true }).then((notes) => {
-	notes.forEach((note) => {
-		console.log(note);
-	});
+note.save().then((result) => {
+	console.log("note saved!");
 	mongoose.connection.close();
 });
+
+// Note.find({ important: true }).then((notes) => {
+// 	notes.forEach((note) => {
+// 		console.log(note);
+// 	});
+// 	mongoose.connection.close();
+// });
